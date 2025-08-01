efa725e8b19386103d91520363aa07fc001d7701
adelhardt <celina.adelhardt@em.ag>
2025-08-01 08:00:24 +0000

    feat(aid_parser): implement extraction of the topics in the aid sm

--- python-connector/core/aid_parser.py
+++ python-connector/core/aid_parser.py
@@ -1,17 +1,30 @@
 """This module provides functions to parse AID Submodels and extract MQTT interface descriptions."""
 
-from basyx.aas import model
-from basyx.aas.model import NamespaceSet, Reference, Submodel, SubmodelElement, SubmodelElementCollection
+from collections.abc import Iterator
+
+from basyx.aas.model import (
+    ExternalReference,
+    Key,
+    KeyTypes,
+    NamespaceSet,
+    Property,
+    Reference,
+    Submodel,
+    SubmodelElement,
+    SubmodelElementCollection,
+)
 from basyx.aas.util import traversal
 
-SEMANTIC_ID_INTERFACE: str = "https://admin-shell.io/idta/AssetInterfacesDescription/1/0/Interface"
-SEMANTIC_ID_MQTT: str = "http://www.w3.org/2011/mqtt"
+
 class AIDParser:
     """A class to handle parsing of AID Submodels and connecting to MQTT topics."""
+
+    topics: dict[str, str]
 
     _aid_sm: Submodel
     _mqtt_interface: SubmodelElementCollection = None
     _interaction_properties: list = None
+    _base_url: str = ""
 
 
     def __init__(self, aid_sm: Submodel):
@@ -21,20 +34,58 @@ class AIDParser:
         """
         self._aid_sm = aid_sm
 
-        mqtt_interface: SubmodelElementCollection = find_by_semantic_id(aid_sm.submodel_element, SEMANTIC_ID_INTERFACE)
+        mqtt_interface: SubmodelElementCollection = self._find_mqtt_interface()
         if mqtt_interface is None:
             raise ValueError("MQTT interface description not found in AID Submodel.")
         self._mqtt_interface = mqtt_interface
- 
-        interaction_metadata: SubmodelElementCollection = find_by_supplemental_semantic_id(
-            self._mqtt_interface.value, SEMANTIC_ID_MQTT
+
+        self._get_base_url()
+
+        interaction_metadata: SubmodelElementCollection = find_by_semantic_id(
+            self._mqtt_interface.value, "https://admin-shell.io/idta/AssetInterfacesDescription/1/0/InteractionMetadata"
         )
         if interaction_metadata is None:
             raise ValueError("InteractionMetadata SMC not found in MQTT interface description.")
 
-        self._interaction_properties = self._loop_property_definitions(interaction_metadata)
+        self._interaction_properties = self._extract_topics(interaction_metadata)
 
-    def _loop_property_definitions(self, interaction_metadata: SubmodelElementCollection):
+    def _find_mqtt_interface(self) -> SubmodelElementCollection:
+        """Find the MQTT interface collection in the AID Submodel by semantic_id and supplemental_semantic_id.
+
+        :return: The MQTT interface collection if found, otherwise None.
+        """
+        interfaces: list[SubmodelElement] = find_all_by_semantic_id(
+            self._aid_sm.submodel_element, "https://admin-shell.io/idta/AssetInterfacesDescription/1/0/Interface"
+        )
+
+        mqtt_ref: Reference = ExternalReference(
+            [Key(
+                type_ = KeyTypes.GLOBAL_REFERENCE,
+                value = "http://www.w3.org/2011/mqtt"
+            )]
+        )
+        return next(interface for interface in interfaces if isinstance(interface, SubmodelElementCollection) and
+                           interface.supplemental_semantic_id.__contains__(mqtt_ref)) if interfaces else None
+
+
+    def _get_base_url(self):
+        """Set the base URL for the MQTT interface from the EndpointMetadata SMC."""
+        endpoint_metadata: SubmodelElementCollection = find_by_semantic_id(
+            self._mqtt_interface.value, "https://admin-shell.io/idta/AssetInterfacesDescription/1/0/EndpointMetadata"
+        )
+        if endpoint_metadata is None:
+            raise ValueError("EndpointMetadata SMC not found in AID Submodel.")
+        base: Property = find_by_semantic_id(
+            endpoint_metadata.value, "https://www.w3.org/2019/wot/td#base"
+        )
+        if base is None:
+            raise ValueError("BaseUrl Property not found in EndpointMetadata SMC.")
+        if not isinstance(base, Property):
+            raise TypeError("BaseUrl is not a Property type.")
+        self._base_url = base.value
+
+
+    def _extract_topics(self, interaction_metadata: SubmodelElementCollection):
         """
         Loop through the MQTT interface collection and find all Interactionmetadata.property elements.
 
@@ -44,22 +95,69 @@ class AIDParser:
         mqtt_property_collection: SubmodelElementCollection = find_by_semantic_id(
             interaction_metadata.value, "https://www.w3.org/2019/wot/td#PropertyAffordance"
         )
-        # TODO
+        if mqtt_property_collection is None:
+            raise ValueError("PropertyAffordance SMC not found in InteractionMetadata SMC.")
+        if not isinstance(mqtt_property_collection, SubmodelElementCollection):
+            raise TypeError("PropertyAffordance is not a SubmodelElementCollection type.")
 
+        property_definitions: list[SubmodelElementCollection] = [
+            prop_def for prop_def in find_all_by_semantic_id(
+                traversal.walk_submodel(mqtt_property_collection),
+                "https://admin-shell.io/idta/AssetInterfaceDescription/1/0/PropertyDefinition"
+            )
+            if isinstance(prop_def, SubmodelElementCollection) and
+            find_by_semantic_id(prop_def.value, "https://www.w3.org/2019/wot/td#hasForm") is not None
+        ]
 
-    def _find_topics(self):
-        pass
+        self._find_topics(property_definitions)
+
+    def _find_topics(self, property_definitions: list[SubmodelElementCollection]):
+        for prop_def in property_definitions:
+            forms = find_by_semantic_id(
+                prop_def.value, "https://www.w3.org/2019/wot/td#hasForm"
+            )
+
+            target_href = find_by_semantic_id(
+                forms.value, "https://www.w3.org/2019/wot/hypermedia#hasTarget"
+            )
+
+            if target_href is None:
+                raise ValueError("TargetHref not found in Form SMC.")
+            if not isinstance(target_href, Property):
+                raise TypeError("TargetHref is not a Property type.")
+
+            self._topics[prop_def.id_short] = target_href.value
+
+        
+
+def find_all_by_semantic_id(parent: Iterator[SubmodelElement], semantic_id_value: str) -> list[SubmodelElement]:
+    """Find all SubmodelElements having a specific Semantic ID.
+
+    :param parent: The NamespaceSet to search within.
+    :param semantic_id_value: The semantic ID value to search for.
+    :return: The found SubmodelElement(s) or an empty list if not found.
+    """
+    reference: Reference = ExternalReference(
+        [Key(
+            type_= KeyTypes.GLOBAL_REFERENCE,
+            value=semantic_id_value
+        )]
+    )
+    found_elements: list[SubmodelElement] = [
+        element for element in parent if element.semantic_id.__eq__(reference)
+    ]
+    return found_elements
 
 def find_by_semantic_id(parent: NamespaceSet[SubmodelElement], semantic_id_value: str) -> SubmodelElement:
     """Find a SubmodelElement by its semantic ID.
 
     :param parent: The NamespaceSet to search within.
     :param semantic_id_value: The semantic ID value to search for.
-    :return: The found SubmodelElement, or None if not found.
+    :return: The first found SubmodelElement, or None if not found.
     """
-    reference: Reference = model.ExternalReference(
-        [model.Key(
-            type_= model.KeyTypes.GLOBAL_REFERENCE,
+    reference: Reference = ExternalReference(
+        [Key(
+            type_= KeyTypes.GLOBAL_REFERENCE,
             value=semantic_id_value
         )]
     )
@@ -73,11 +171,11 @@ def find_by_supplemental_semantic_id(parent: NamespaceSet[SubmodelElement], sema
 
     :param parent: The NamespaceSet to search within.
     :param semantic_id_value: The supplemental semantic ID value to search for.
-    :return: The found SubmodelElement, or None if not found.
+    :return: The first found SubmodelElement, or None if not found.
     """
-    reference: Reference = model.ExternalReference(
-        [model.Key(
-            type_= model.KeyTypes.GLOBAL_REFERENCE,
+    reference: Reference = ExternalReference(
+        [Key(
+            type_= KeyTypes.GLOBAL_REFERENCE,
             value=semantic_id_value
         )]
     )
