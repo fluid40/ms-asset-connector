a1380dd42fe338d7357018f108f7275418e0dd08
Tom Gneuß <tom.gneuss@tu-dresden.de>
2025-09-30 13:04:18 +0200

    implement opcua connect

--- python_connector/opc_ua/opcua_asset_connector.py
+++ python_connector/opc_ua/opcua_asset_connector.py
@@ -1,3 +1,4 @@
+import opcua
 from basyx.aas.model import ModelReference, SubmodelElementCollection
 
 from python_connector.core.asset_connector import IAssetConnector
@@ -5,14 +6,19 @@ from python_connector.core.asset_connector import IAssetConnector
 
 class OpcuaAssetConnector(IAssetConnector):
 
-    def __init__(self, aid_id: str, interface_smc: SubmodelElementCollection):  # noqa: D107
+    def __init__(self, aid_id: str, interface_smc: SubmodelElementCollection):
         self._aid_id = aid_id
         self._interface = interface_smc
         super().__init__(aid_id, interface_smc)
 
     def connect(self):
-        # no connection needed -> just request values on demand
-        pass
+        self._client = opcua.Client(self._base)
+
+        # TODO: set based on self._auth
+        # self._client.set_user()
+        # self._client.set_password()
+
+        self._client.connect()
 
     def get_value(self, endpoint_reference: ModelReference) -> str:
         return super().get_value(endpoint_reference)
