f93e268366b98775e28f90f38217af35ff8766ac
Tom Gneuß <tom.gneuss@tu-dresden.de>
2025-10-01 08:26:38 +0200

    implement get_value in OpcuaAssetConnector

--- python_connector/opc_ua/opcua_asset_connector.py
+++ python_connector/opc_ua/opcua_asset_connector.py
@@ -1,27 +1,51 @@
-import opcua
+import json
+
+from opcua import Client
 from basyx.aas.model import ModelReference, SubmodelElementCollection
 
+from python_connector.core.aid_interface_parser import construct_idshort_path_from_reference
 from python_connector.core.asset_connector import IAssetConnector
 
 
 class OpcuaAssetConnector(IAssetConnector):
 
     def __init__(self, aid_id: str, interface_smc: SubmodelElementCollection):
+        self._client = None
+        self._is_connected = False
         self._aid_id = aid_id
         self._interface = interface_smc
         super().__init__(aid_id, interface_smc)
 
     def connect(self):
-        self._client = opcua.Client(self._base)
+        try:
+            self._client = Client(self._base)
+            self._client.connect()
+            self._is_connected = True
+        except Exception as e:
+            print(e)
 
-        # TODO: set based on self._auth
-        # self._client.set_user()
-        # self._client.set_password()
+    async def get_value(self, endpoint_reference: ModelReference) -> str | None:
+        """Get the value for a specific model reference."""
 
-        self._client.connect()
+        property_idshort_path = construct_idshort_path_from_reference(endpoint_reference)
+        node_id = self._property_to_href_map[property_idshort_path]["href"]
+        keys = self._property_to_href_map[property_idshort_path]["keys"]
 
-    def get_value(self, endpoint_reference: ModelReference) -> str:
-        return super().get_value(endpoint_reference)
+        value_in_payload = None
+        if self._is_connected:
+            node = self._client.get_node(node_id)
+            value_in_payload = node.get_value()
 
-    def set_value(self, endpoint_reference: ModelReference, *args):
-        super().set_value(endpoint_reference, *args)
+        if value_in_payload is None:
+            return None
+
+        # using the keys of the potentially nested properties, dive into the complex JSON object (MQTT payload)
+        # to retrieve the requested value
+        for k in keys:
+            value_in_payload = json.dumps(json.loads(value_in_payload)[k])
+
+        result = value_in_payload
+        return str(result)
+
+    async def set_value(self, endpoint_reference: ModelReference, *args):
+        await super().set_value(endpoint_reference, *args)
